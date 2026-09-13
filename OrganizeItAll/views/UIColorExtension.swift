a76ac7d4791ddc082660437a8d09aa1bfaba8fe9
moyarich <moyarich@hotmail.com>
2026-09-13 17:11:11 -0500

    Remove legacy UIKit and Core Data files

--- OrganizeItAll/views/UIColorExtension.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-import UIKit
-
-extension Color {
-    static let brandAccent = Color(red: 0.20, green: 0.42, blue: 0.78)
-    static let cardBackground = Color(UIColor.secondarySystemBackground)
-}
